be39f6bb6127f691561b1786bd4df77774dfbbb7
yumejensen <yumejensen@gmail.com>
2025-09-11 19:41:30 -0500

    Added webpack and loaders, attempted to configure (webpack build failing atm

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/client/index.tsx":
+/*!******************************!*\
+  !*** ./src/client/index.tsx ***!
+  \******************************/
+/***/ (() => {
+
+eval("{throw new Error(\"Module build failed (from ./node_modules/@babel/preset-typescript/lib/index.js):\\nTypeError: Cannot read properties of undefined (reading 'slice')\\n    at throwVersionError (/home/yoomeh/senior/crooked-curators/node_modules/@babel/helper-plugin-utils/lib/index.js:61:15)\\n    at Object.assertVersion (/home/yoomeh/senior/crooked-curators/node_modules/@babel/helper-plugin-utils/lib/index.js:10:5)\\n    at /home/yoomeh/senior/crooked-curators/node_modules/@babel/preset-typescript/lib/index.js:112:7\\n    at Object.<anonymous> (/home/yoomeh/senior/crooked-curators/node_modules/@babel/helper-plugin-utils/lib/index.js:32:12)\");\n\n//# sourceURL=webpack://crooked-curators/./src/client/index.tsx?\n}");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/client/index.tsx"]();
+/******/ 	
+/******/ })()
+;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/client/index.tsx",
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  resolve: {
+    modules: [__dirname, "src", "node_modules"],
+    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: ["ts-loader", "babel-loader", "@babel/preset-react", "@babel/preset-typescript"]
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        exclude: /node_modules/,
+        use: ["file-loader"]
+      }, 
+    ],
+  },
+};
