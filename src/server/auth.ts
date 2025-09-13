ffdf7c125ebce4af40d09a237c6d8f06107e27d5
yumejensen <yumejensen@gmail.com>
2025-09-13 12:02:17 -0500

    Added require to app.ts, database connection string appears with npm start now - commenting out auth.js for now (working through bug)

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,17 +1,17 @@
-const GoogleStrategy = require( 'passport-google-oauth20' ).Strategy;
+// const GoogleStrategy = require( 'passport-google-oauth20' ).Strategy;
 
-require('dotenv').config();
+// require('dotenv').config();
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
+// const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
 
-// implement new google strategy: updates/creates user upon login
-passport.use(new GoogleStrategy({
-  clientId: GOOGLE_CLIENT_ID,
-  clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3000/auth/google/callback',
-},
-  function(accessToken, refreshToken, profile, done){
+// // implement new google strategy: updates/creates user upon login
+// passport.use(new GoogleStrategy({
+//   clientId: GOOGLE_CLIENT_ID,
+//   clientSecret: GOOGLE_CLIENT_SECRET,
+//   callbackURL: 'http://localhost:3000/auth/google/callback',
+// },
+//   function(accessToken, refreshToken, profile, done){
 
-    // access users schema with findOrCreate
-  }
-))
+//     // access users schema with findOrCreate
+//   }
+// ))
