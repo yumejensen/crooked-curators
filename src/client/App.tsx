f686b72fd07d199723a5d865d92ac895f7df49ca
avaAlley <ava.rose.alley@gmail.com>
2025-09-15 10:43:54 -0500

    Added mantine theming

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,19 +1,69 @@
 import * as React from "react";
+import { createTheme, MantineProvider } from '@mantine/core';
 
-// interface AppProps {
-//     foo: number;
-//     bar: string;
-// }
+import Homepage from "./Views/Homepage";
 
-// const App: React.FC<AppProps> = (props) => {
-//     return <>{ props.foo }</>
-    
-// }
+const theme = createTheme({
+    // theme here
+    colors: {
+        // add all colors
+
+        pink: [
+            "#3B262C", // OG
+            "#800933", // OG
+            "#C25576",
+            "#603E48",
+            "#9E6072", // OG
+            "#B58694",
+            "#CCACB6",
+            "#E3D2D7",
+            "#FAF8F9",
+            "#FFFFFF"
+        ],
+
+        yellow: [
+            "#736A47",
+            "#998D5E",
+            "#B2A882",
+            "#EDDB98", // OG
+            "#F7EABA",
+            "#DED6BA",
+            "#F0E7CA", // OG
+            "#F6EDCC",
+            "#F2EFE3",
+            "#FFFFFF",
+        ],
+
+        teal: [
+            "#003020",
+            "#0B523A",
+            "#128860",
+            "#19BE86",
+            "#30E4A7",
+            "#00FCA7",
+            "#7BE3C0", // OG
+            "#ADEED8",
+            "#E0F9F0",
+            "#FFFFFF",
+        ]
+    },
+    shadows: {
+        md: '1px 1px 3px rgba(0, 0, 0, .25)',
+        xl: '5px 5px 3px rgba(0, 0, 0, .25)',
+    },
+    headings: {
+        fontFamily: 'Roboto, sans-serif',
+        sizes: {
+            h1: { fontSize: '36px' },
+        },
+    }
+});
+
 
 export default function App () {
     return (
-        <div>
-            Hello Welcome to Crooked Curators!
-        </div>
+        <MantineProvider theme={theme}>
+            <Homepage />
+        </MantineProvider>
     )
 }
