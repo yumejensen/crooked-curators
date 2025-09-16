c6cc40f4028f9b79fee667e4d3ed1552ae2a4916
avaAlley <ava.rose.alley@gmail.com>
2025-09-16 16:33:54 -0500

    Removed mantine components from homepage

--- src/client/Views/Homepage.tsx
+++ src/client/Views/Homepage.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-import NavbarMinimal from "../Navbar/Navbar";
-
 export default function Homepage() {
 
   return (
     <>
-      <NavbarMinimal />
+
     </>
   )
 
