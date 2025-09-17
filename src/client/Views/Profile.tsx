69dd13ec49db10825d7de51e5b739a04fa6a8150
avaAlley <ava.rose.alley@gmail.com>
2025-09-17 14:56:38 -0500

    Added React.FC basic for Profile view

---
+++ src/client/Views/Profile.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// COMPONENTS
+
+
+const Profile: React.FC = () => {
+    return(
+        <div>
+            <p>Replace with user avatar</p>
+            <p>Replace with username</p>
+            <p>Replace with ribbons</p>
+            <p>Replace with artworks/galleries</p>
+        </div>
+    )
+}
+
+export default Profile;
