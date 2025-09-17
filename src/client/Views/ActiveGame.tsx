8f1be662b04dd917d0a576044d2c5207fdcd91da
avaAlley <ava.rose.alley@gmail.com>
2025-09-17 14:55:31 -0500

    Added placeholder components

--- src/client/Views/ActiveGame.tsx
+++ src/client/Views/ActiveGame.tsx
@@ -1,9 +1,18 @@
 // The Active Game View that holds that canvas and reference image
 
+import React from 'react';
+
+// COMPONENTS
+import Canvas from '../Components/Canvas';
+
 const ActiveGame: React.FC = () => {
     return(
         <div>
             <p>This is the active game!</p>
+            <p>Replace with half of avatars</p>
+            <p>Replace with Reference</p>
+            <p>Replace with half of avatars</p>
+            <Canvas />
         </div>
     )
 }
