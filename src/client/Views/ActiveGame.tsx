42af5b95dd443abeccffd686dc36bf646ab550fa
yumejensen <yumejensen@gmail.com>
2025-09-18 12:05:20 -0500

    Added cards columns

--- src/client/Views/ActiveGame.tsx
+++ src/client/Views/ActiveGame.tsx
@@ -2,56 +2,48 @@
 
 import React from 'react';
 
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
+import { Layout } from 'antd';
 
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
-
-const { Header, Content, Footer, Sider } = Layout;
+import { Card, Col, Row } from 'antd';
 
 // COMPONENTS
 import Canvas from '../Components/Canvas';
 
-const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
-  (icon, index) => {
-    const key = String(index + 1);
-
-    return {
-      key: `sub${key}`,
-      icon: React.createElement(icon),
-      label: `subnav ${key}`,
-      children: Array.from({ length: 4 }).map((_, j) => {
-        const subKey = index * 4 + j + 1;
-        return {
-          key: subKey,
-          label: `option${subKey}`,
-        };
-      }),
-    };
-  },
-);
 
 const ActiveGame: React.FC = () => {
-    const {
-        token: { colorBgContainer, borderRadiusLG },
-    } = theme.useToken();
+
 
     return(
-    <>
-        <Layout style={{ padding: '24px 0', background: colorBgContainer, borderRadius: borderRadiusLG }}>
-            <Sider style={{ background: 'blue' }} width={200}>
-                <Menu
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                style={{ height: '100%' }}
-                items={items2}
-                />              
-            </Sider>
+    
+      <Row gutter={2}>
+        <Col span={5}>
+          <Card title="Col 1" variant="borderless">
+            Card content
+          </Card>
+        </Col>
+        <Col span={14}>
+          <Card title="Col 2" variant="borderless">
             <Canvas />
-        </Layout>
-    </>
+          </Card>
+        </Col>
+        <Col span={5}>
+          <Card title="Col 3" variant="borderless">
+            Card content
+          </Card>
+        </Col>
+      </Row>
+      
     )
 }
 
 export default ActiveGame;
+
+            // <Sider style={{ background: 'blue' }} width={200}>
+            //     <Menu
+            //     mode="inline"
+            //     defaultSelectedKeys={['1']}
+            //     defaultOpenKeys={['sub1']}
+            //     style={{ height: '100%' }}
+            //     items={items2}
+            //     />              
+            // </Sider>
