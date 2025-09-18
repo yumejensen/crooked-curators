df224d5419e7e17acda0d1195997233ca560384d
joseph <josephpfurman@gmail.com>
2025-09-18 11:57:54 -0500

    ADD: Curator Search component, includes an input and image

--- src/client/Components/CuratorSearch.tsx
+++ src/client/Components/CuratorSearch.tsx
@@ -1,1 +1,31 @@
 // Contains the Reference and ReferenceSearch components
+
+import React from 'react'
+import {
+  Divider,
+  Col,
+  Row,
+  Flex,
+  FlexProps,
+  Segmented
+} from 'antd';
+
+import ReferenceSearch from './ReferenceSearch'
+import Reference from './Reference'
+
+
+const CuratorSearch = () => {
+
+  return(
+    <Flex gap="middle" align="center" vertical>
+      <Divider>
+        <ReferenceSearch />
+      </Divider>
+      <Divider>
+        <Reference />
+      </Divider>
+    </Flex>
+  )
+}
+
+export default CuratorSearch
--- src/client/Views/GameSettings.tsx
+++ src/client/Views/GameSettings.tsx
@@ -2,12 +2,16 @@
 
 import React from 'react';
 
+//Temporarily including Curator Search here
+import CuratorSearch from '../Components/CuratorSearch'
+
 // COMPONENTS
 
 
 const GameSettings: React.FC = () => {
     return(
         <div>
+            <CuratorSearch />
             <p>Replace with user list</p>
             <p>Replace with game settings</p>
             <p>Replace with start game button</p>
