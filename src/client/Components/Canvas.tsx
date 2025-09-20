fc4f805fff457d112dac7808d342cbdce40058d2
avaAlley <ava.rose.alley@gmail.com>
2025-09-20 11:59:39 -0500

    Color picker no longer changes all line colors. New strokes can be made with new color

--- src/client/Components/Canvas.tsx
+++ src/client/Components/Canvas.tsx
@@ -41,7 +41,7 @@ const Canvas = () => {
 
   const [tool, setTool] = React.useState('pen');
   const [lines, setLines] = React.useState([]);
-  const [lineColor, setLineColor] = React.useState("#df4b26");
+  const [lineColor, setLineColor] = React.useState("#000000");
 
   const [justify, setJustify] = useState<FlexProps['justify']>('space-evenly');
   const [alignItems, setAlignItems] = useState<FlexProps['align']>('center');
@@ -53,7 +53,7 @@ const Canvas = () => {
   const handleMouseDown = (e) => {
     isDrawing.current = true;
     const pos = e.target.getStage().getPointerPosition();
-    setLines([...lines, { tool, points: [pos.x, pos.y] }]);
+    setLines([...lines, { tool, points: [pos.x, pos.y], stroke: lineColor }]);
   };
 
   const handleMouseMove = (e) => {
@@ -66,7 +66,7 @@ const Canvas = () => {
     let lastLine = lines[lines.length - 1];
     // add point
     lastLine.points = lastLine.points.concat([point.x, point.y]);
-    
+
     // replace last
     lines.splice(lines.length - 1, 1, lastLine);
     setLines(lines.concat());
@@ -101,7 +101,7 @@ const Canvas = () => {
                 <Line
                   key={i}
                   points={line.points}
-                  stroke={lineColor}
+                  stroke={line.stroke}
                   strokeWidth={5}
                   tension={0.5}
                   lineCap="round"
