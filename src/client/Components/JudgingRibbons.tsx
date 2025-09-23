7c6484f2c4d7df38c164bb07353e0de679e56eb2
avaAlley <ava.rose.alley@gmail.com>
2025-09-22 19:09:41 -0500

    Created judging ribbons component

---
+++ src/client/Components/JudgingRibbons.tsx
@@ -0,0 +1,10 @@
+// Displays 3 random ribbons pulled from the database. One blue, one red, one white.
+
+import React from 'react';
+import { useState } from 'react';
+
+const JudgingRibbons: React.FC = (props) => {
+  
+}
+
+export default JudgingRibbons;
