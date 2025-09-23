98c9a8df79c56871488394703ff90d1252013a19
avaAlley <ava.rose.alley@gmail.com>
2025-09-23 15:57:39 -0500

    Added s3 handling for server

--- src/server/routes/s3-storage.ts
+++ src/server/routes/s3-storage.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import { generateUploadURL } from '../s3'
 
 const s3UrlRouter = Router();
 
-s3UrlRouter.get("/", (req, res) => {
-
+s3UrlRouter.get("/", async (req, res) => {
+  const url = await generateUploadURL()
+  res.send({url});
 })
 
 export { s3UrlRouter };
