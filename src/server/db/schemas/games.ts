5bd10281b169d759b561a3f7910d9ea88b6521f6
avaAlley <ava.rose.alley@gmail.com>
2025-09-27 13:31:54 -0500

    edited games schema

--- src/server/db/schemas/games.ts
+++ src/server/db/schemas/games.ts
@@ -2,15 +2,23 @@ import sequelize from "../index";
 import { DataTypes } from "sequelize";
 
 import { User } from "./users";
+import { User_Game } from './users-games';
 
 const Game = sequelize.define("game", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
   gameCode: {
     type: DataTypes.STRING,
     allowNull: true,
   },
 });
 
-Game.belongsToMany(User, { through: "users_games" });
+// establish relationships
+Game.belongsToMany(User, { through: User_Game });
 
 // synchronize model to the db
 (async () => {
