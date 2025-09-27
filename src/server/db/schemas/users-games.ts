10edfc5adbb23791913e7447683c5b0967f60777
avaAlley <ava.rose.alley@gmail.com>
2025-09-27 13:32:14 -0500

    created users_games schema

---
+++ src/server/db/schemas/users-games.ts
@@ -0,0 +1,30 @@
+import sequelize from "../index";
+import { DataTypes } from "sequelize";
+
+import { User } from "./users";
+import { Game } from './games'
+
+const User_Game = sequelize.define('user_game', {
+  joinedAt: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User,
+      key: 'id'
+    },
+    allowNull: false
+  },
+  game_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Game,
+      key: 'id'
+    },
+    allowNull: false
+  },
+}, { timestamps: false });
+
+export { User_Game };
