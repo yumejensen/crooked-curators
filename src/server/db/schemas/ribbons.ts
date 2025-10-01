35b3c1b680ab2c103aa3cca683020d5ae7bc22f7
avaAlley <ava.rose.alley@gmail.com>
2025-10-01 11:16:31 -0500

    Added cols to ribbon model

--- src/server/db/schemas/ribbons.ts
+++ src/server/db/schemas/ribbons.ts
@@ -1,13 +1,29 @@
 import sequelize from "../index";
 import { DataTypes } from 'sequelize';
 
-const Ribbon = sequelize.define('ribbon'. {
+const Ribbon = sequelize.define('ribbon', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true,
     allowNull: false
   },
+  color: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  points: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  source: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
 })
 
 export { Ribbon };
