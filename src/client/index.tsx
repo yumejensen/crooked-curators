b843bf410f9844a284cbb0cd520a428334808c4f
avaAlley <ava.rose.alley@gmail.com>
2025-10-15 11:17:35 -0500

    Moved config provider to index.tsx

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,12 +2,32 @@ import * as React from "react";
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from './App'
+import AppCopy from "./AppCopy";
 import ActiveGame from './Views/ActiveGame';
+
+import {
+  ConfigProvider,
+} from "./antdComponents";
 
 const app = createRoot(document.getElementById('root'))
 
 app.render(
   <BrowserRouter>
-    <App />
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token
+          colorPrimary: "#058f6fff",
+          colorBgLayout: "#e0b5c2",
+          borderRadius: 2,
+
+          // Alias Token
+          colorBgContainer: "#f8f4e5",
+        },
+      }}
+    >
+
+      <App />
+    </ConfigProvider>
   </BrowserRouter>
 );
