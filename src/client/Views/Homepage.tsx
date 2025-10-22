465ffe1ee3f85a740ea58d5d8c172e7490c241e1
yumejensen <yumejensen@gmail.com>
2025-10-22 12:32:27 -0500

    Layout with title/ join on left and instructions right

--- src/client/Views/Homepage.tsx
+++ src/client/Views/Homepage.tsx
@@ -1,50 +1,41 @@
 // Page that holds the choose avatar, join game, create a game menu
 
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import axios from 'axios';
-import crookedCuratorsTitle from '../../assets/images/Crooked_Curators_Title.png';
-import monaLoser from '../../assets/images/Mona_Loser_Frame.png'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import crookedCuratorsTitle from "../../assets/images/Crooked_Curators_Title.png";
+import monaLoser from "../../assets/images/Mona_Loser_Frame.png";
 
-import {
-  Flex,
-  Row,
-  Col,
-} from '../antdComponents';
+import { Flex, Row, Col } from "../antdComponents";
 
-import HomepageSignInToPlay from '../Components/HomepageSignInToPlay';
-import HomepageCreateJoin from '../Components/HomepageCreateJoin';
-import HomepageName from '../Components/HomepageName';
-import HowToPlay from '../Components/HowToPlay';
+import HomepageSignInToPlay from "../Components/HomepageSignInToPlay";
+import HomepageCreateJoin from "../Components/HomepageCreateJoin";
+import HomepageName from "../Components/HomepageName";
+import HowToPlay from "../Components/HowToPlay";
 
 // styling
 
 const largeStyle: React.CSSProperties = {
-  width: '100%',
-  height: '100%',
-  borderRadius: 6
+  width: "100%",
+  height: "100%",
+  borderRadius: 6,
 };
 
 const titleStyle: React.CSSProperties = {
-  width: '100%',
-  height: '100%',
-  borderRadius: 6
+  width: "100%",
+  height: "100%",
+  borderRadius: 6,
 };
 
-
-
 const Homepage: React.FC = () => {
-
   // --------------------[STATES]---------------------
-  
-  const [randomName, setRandomName] = useState('');
 
+  const [randomName, setRandomName] = useState("");
 
   // --------------------[HANDLERS]--------------------
 
-
   const handleRandomizeName = () => {
-    axios.get('/name-randomizer').then(res => {
+    axios.get("/name-randomizer").then((res) => {
       setRandomName(res.data);
     });
   };
@@ -56,49 +47,39 @@ const Homepage: React.FC = () => {
   // --------------------[RENDER]---------------------
 
   return (
-    <Flex>
-      <Flex style={largeStyle} justify="center" align='flex-start'>
-        <Col>
-          <Row>
-            <Flex style={titleStyle} justify="center" align='flex-center' vertical>
-              <img
-                src={crookedCuratorsTitle}
-                alt="Crooked Curators"
-                width={320}
-                style={{
-                  display:"block",
-                  marginLeft:"auto",
-                  marginRight:"auto",
-                  marginBottom: 10
-                }} 
-              />
-              <img
-                src={monaLoser}
-                alt="Mona Loser"
-                width={300}
-                style={{
-                  display:"block",
-                  marginLeft:"auto",
-                  marginRight:"auto"
-                }}
-              />
-            </Flex>
-          </Row>
-          <Row gutter={15}>
-            <HomepageName 
+    
+      <Flex style={largeStyle} justify="center" align="flex-start">
+        <Row gutter={[16, 16]}>
+          <Col >
+            <img
+              src={crookedCuratorsTitle}
+              alt="Crooked Curators"
+              width={320}
+              style={{
+                marginBottom: 10,
+              }}
+            />
+            <img src={monaLoser} alt="Mona Loser" width={300} />
+          </Col>
+
+          <Col >
+            <HomepageName
               randomName={randomName}
               handleRandomizeName={handleRandomizeName}
             />
-    
+
             <HomepageCreateJoin randomName={randomName} />
             <HomepageSignInToPlay />
-          </Row>
-          <Row>
-            <HowToPlay />
-          </Row>
-        </Col>
+          </Col>
+
+        </Row>
+
+        <Row>
+          <HowToPlay />
+        </Row>
+
       </Flex>
-    </Flex>
+    
   );
 };
 
