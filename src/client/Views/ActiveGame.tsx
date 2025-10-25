be8f8e840710aefbd0828d9292910fb804309de2
avaAlley <ava.rose.alley@gmail.com>
2025-10-25 18:55:03 -0500

    Removed columns from active game

--- src/client/Views/ActiveGame.tsx
+++ src/client/Views/ActiveGame.tsx
@@ -9,41 +9,23 @@ import {
 // COMPONENTS
 import Canvas from '../Components/Canvas';
 import Reference from '../Components/Reference';
-import ArtSubmitCount from '../Components/ArtSubmitCount';
-
 
 const ActiveGame: React.FC = () => {
-
 
   // --------------------[RENDER]---------------------
 
   return (
     <>
-      <Row gutter={2}>
-        <Col span={3}>
-         
-        </Col>
-        <Col span={18}>
-
-            <Flex gap="middle" align="center" vertical>
-              <Flex
-                style={{width: 500}}
-                justify='space-evenly'
-                align='center'
-              >
-                <Reference />
-              </Flex>
-            </Flex>
-            <Canvas />
-
-        </Col>
-
-        <Col span={3}>
-
-            <ArtSubmitCount />
-
-        </Col>
-      </Row>
+      <Flex gap="middle" align="center" vertical>
+        <Flex
+          style={{ width: 500 }}
+          justify='space-evenly'
+          align='center'
+        >
+          <Reference />
+        </Flex>
+      </Flex>
+      <Canvas />
     </>
   );
 };
