2d29e9f399063136e789ec37c4c971c78082e889
yumejensen <yumejensen@gmail.com>
2025-10-25 18:26:39 -0500

    Added styling to buttons

--- src/client/Components/ToJudging.tsx
+++ src/client/Components/ToJudging.tsx
@@ -4,9 +4,7 @@ import { Button } from "../antdComponents";
 
 import { useSocketContext, useGameContext } from "../context";
 
-
 const ToJudging = () => {
-  
   const { socket } = useSocketContext();
   const { doneCount, players } = useGameContext().game;
 
@@ -16,15 +14,22 @@ const ToJudging = () => {
 
   // --------------------[RENDER]---------------------
 
-  
-    return (
-      <Button
-        onClick={handleClick}
-        disabled={doneCount !== players.length - 1}
-      >
-        Judging Time!
-      </Button>
-      );
-}
+  return (
+    <Button
+      onClick={handleClick}
+      disabled={doneCount !== players.length - 1}
+      variant="solid"
+      color="primary"
+      style={{
+        backgroundColor: "var(--nav)",
+        borderRadius: 8,
+        paddingBlock: 20,
+        paddingInline: 30,
+      }}
+    >
+      Judging Time!
+    </Button>
+  );
+};
 
 export default ToJudging;
