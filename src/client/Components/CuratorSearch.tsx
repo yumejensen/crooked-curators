714b7a6b0a43c0921e36f036d2feab2086bf13c4
yumejensen <yumejensen@gmail.com>
2025-10-25 18:54:51 -0500

    Changed styling in CuratorSearch

--- src/client/Components/CuratorSearch.tsx
+++ src/client/Components/CuratorSearch.tsx
@@ -41,82 +41,81 @@ const CuratorSearch = () => {
     socket?.emit("curatorSelect", results[selected]);
     setConfirmed(true);
   };
-  // return (
-  //   <Flex gap="middle" align="center" vertical>
-  //     <Flex>
-  //       <div>
-  //       <ReferenceSearch handleSearch={handleSearch} disabled={results.length}/>
-  //       <p>{(results.length > 0 && results.length < 4) ? `That search didn't get enough results, you can try again` : `Choose a Reference!`}</p>
-  //       </div>
-  //       <ArtSubmitCount />
-  //     </Flex>
-  // <Divider>
-  //   <Reference {...results[selected]} />
-  //   <Button onClick={nextResult} disabled={selected >= results.length - 1}>
-  //     {results.length === 0
-  //       ? "Search For Some Inspiration!"
-  //       : `Result ${selected + 1} out of ${results.length}`}
-  //   </Button>
-  //   <Popconfirm
-  //     title="Are you sure to select this reference?"
-  //     onConfirm={selectReference}
-  //     onCancel={() => {}}
-  //     okText="Yes"
-  //     cancelText="No"
-  //   >
-  //   <Button disabled={confirmed || results.length === 0}>{confirmed ? `Reference Selected!` : `Choose this Piece`}</Button>
-  //   </Popconfirm>
-  // </Divider>
-  //   </Flex>
-  // );
   return (
-    <>
-      <Row gutter={2}>
-        <Col span={3}></Col>
-        <Col span={18}>
-          <Flex gap="middle" align="center" vertical>
-            <Flex style={{ width: 500 }} justify="space-evenly" align="center">
-              <ReferenceSearch
-                handleSearch={handleSearch}
-                disabled={results.length}
-              />
-              <p>
-                {results.length > 0 && results.length < 4
-                  ? `That search didn't get enough results, you can try again`
-                  : `Choose a Reference!`}
-              </p>
-            </Flex>
-          </Flex>
-          <Divider>
-            <Reference {...results[selected]} />
-            <Button
-              onClick={nextResult}
-              disabled={selected >= results.length - 1}
-            >
-              {results.length === 0
-                ? "Search For Some Inspiration!"
-                : `Result ${selected + 1} out of ${results.length}`}
-            </Button>
-            <Popconfirm
-              title="Are you sure to select this reference?"
-              onConfirm={selectReference}
-              onCancel={() => {}}
-              okText="Yes"
-              cancelText="No"
-            >
-              <Button disabled={confirmed || results.length === 0}>
-                {confirmed ? `Reference Selected!` : `Choose this Piece`}
-              </Button>
-            </Popconfirm>
-          </Divider>
-        </Col>
+    <Flex gap="middle" align="center" vertical>
 
-        <Col span={3}>
-          <ArtSubmitCount />
-        </Col>
-      </Row>
-    </>
+        <div>
+        <ReferenceSearch handleSearch={handleSearch} disabled={results.length}/>
+        <p>{(results.length > 0 && results.length < 4) ? `That search didn't get enough results, you can try again` : `Choose a Reference!`}</p>
+        </div>
+        <ArtSubmitCount />
+
+
+        <Reference {...results[selected]} />
+        <Button onClick={nextResult} disabled={selected >= results.length - 1}>
+          {results.length === 0
+            ? "Search For Some Inspiration!"
+            : `Result ${selected + 1} out of ${results.length}`}
+        </Button>
+        <Popconfirm
+          title="Are you sure to select this reference?"
+          onConfirm={selectReference}
+          onCancel={() => {}}
+          okText="Yes"
+          cancelText="No"
+        >
+        <Button disabled={confirmed || results.length === 0}>{confirmed ? `Reference Selected!` : `Choose this Piece`}</Button>
+        </Popconfirm>
+
+    </Flex>
   );
+  // return (
+  //   <>
+  //     <Row gutter={2}>
+  //       <Col span={18}>
+  //         <Flex gap="middle" align="center" vertical>
+  //           <Flex style={{ width: 500 }} justify="space-evenly" align="center">
+  //             <ReferenceSearch
+  //               handleSearch={handleSearch}
+  //               disabled={results.length}
+  //             />
+  //             <p>
+  //               {results.length > 0 && results.length < 4
+  //                 ? `That search didn't get enough results, you can try again`
+  //                 : `Choose a Reference!`}
+  //             </p>
+  //           </Flex>
+  //         </Flex>
+  //         <Divider>
+  //           <Reference {...results[selected]} />
+  //           <Button
+  //             onClick={nextResult}
+  //             disabled={selected >= results.length - 1}
+  //           >
+  //             {results.length === 0
+  //               ? "Search For Some Inspiration!"
+  //               : `Result ${selected + 1} out of ${results.length}`}
+  //           </Button>
+  //           <Popconfirm
+  //             title="Are you sure to select this reference?"
+  //             onConfirm={selectReference}
+  //             onCancel={() => {}}
+  //             okText="Yes"
+  //             cancelText="No"
+  //           >
+  //             <Button disabled={confirmed || results.length === 0}>
+  //               {confirmed ? `Reference Selected!` : `Choose this Piece`}
+  //             </Button>
+  //           </Popconfirm>
+  //         </Divider>
+  //       </Col>
+
+  //       <Col span={3}>
+  //         <ArtSubmitCount />
+  //       </Col>
+  //     </Row>
+  //   </>
+  // );
 };
 
 export default CuratorSearch;
