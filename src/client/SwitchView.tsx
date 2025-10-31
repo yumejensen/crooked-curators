3fb5fcdfe0d37cf3c9184124ee7dac2c54c02735
yumejensen <yumejensen@gmail.com>
2025-10-31 13:00:13 -0500

    added pseudocode to SwitchView

--- src/client/SwitchView.tsx
+++ src/client/SwitchView.tsx
@@ -4,11 +4,12 @@ import { Navigate } from 'react-router-dom';
 import { useGameContext } from './context';
 
 const SwitchView: React.FC = ({ view }) => {
+  // the view prop is just for the state change to activate the component
+  // the actual served views are coming from the game context
 
   const { role, stage } = useGameContext().game;
-  //const { role, stage } = view
   
-  // new logic - based on game context 
+  // when the stage is reference, there will be a diverging curator and artist view
   if (stage === 'reference' && role === 'curator'){
     return <Navigate to={'/curator'} /> 
   }
@@ -17,6 +18,7 @@ const SwitchView: React.FC = ({ view }) => {
     return <Navigate to={'/game'} />
   }
 
+  // on the judging and gallery views, everyone is seeing the same thing
   if (stage === 'judging'){
     return <Navigate to={'/judging'} /> 
   }
